b01f71c276d81228cc5570c5e328cc84aef10484
Sax Dhiut <mail@iyxeyl.me>
2024-11-27 17:40:29 +0400

    feat: remove unused ReactPlayer import in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Player } from "@/components/player"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { useState } from "react"
-import ReactPlayer from "react-player"
 
 export default function Home() {
 	const [isPlaying, setIsPlaying] = useState(false)
